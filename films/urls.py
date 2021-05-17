c329a3024a098d2ed6b877836888ea0de3775394
Alexander Hrechenko <alex.grechenko@gmail.com>
2021-05-17 23:52:15 +0300

    comments
    added comments with tree structure in admin page

--- films/urls.py
+++ films/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 from django.views.decorators.csrf import csrf_exempt
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('films/<str:params>/', views.index, name='index'),
     path('films/', views.index, name='index'),
+    path('film/<int:pk>/', views.film_detail, name='film-detail'),
     path('update_db/', views.update, name='update-db'),
     path('search/', csrf_exempt(views.search), name="search"),
 ]
