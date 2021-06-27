c7d6dc30de4108582b30731c40ada241e60ca4d3
Alexander Hrechenko <alex.grechenko@gmail.com>
2021-06-27 12:16:41 +0300

    Edited models

--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
     Form for creating and updating Post model
     """
     title = forms.CharField()
-    category = forms.ModelChoiceField(required=False, queryset=Category.objects.all())
+    # category = forms.ModelChoiceField(required=False, queryset=Category.objects.all())
     image = forms.ImageField(required=False)
     video = forms.FileField(required=False)
     content = forms.CharField(
@@ -16,4 +16,5 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'category', 'content', 'image', 'video']
+        # fields = ['title', 'category', 'content', 'image', 'video']
+        fields = ['title', 'content',]
--- blog/migrations/0002_post_category.py
+++ blog/migrations/0002_post_category.py
@@ -5,7 +5,6 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('blog', '0001_initial'),
     ]
@@ -14,6 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='post',
             name='category',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='blog.category'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE,
+                                    to='blog.category'),
         ),
     ]
